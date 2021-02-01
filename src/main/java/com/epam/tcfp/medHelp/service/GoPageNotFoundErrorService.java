c8e4b22ce5200f80388fa5c0d6fde3fdf8245214
CoffeeAfterWakingUp <76391010+CoffeeAfterWakingUp@users.noreply.github.com>
2021-02-01 16:11:39 +0600

    Service For 404 error added

---
+++ src/main/java/com/epam/tcfp/medHelp/service/GoPageNotFoundErrorService.java
@@ -0,0 +1,22 @@
+package com.epam.tcfp.medHelp.service;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import static com.epam.tcfp.medHelp.util.constants.PageName.PAGE_NOT_FOUND_ERROR_PAGE;
+
+public class GoPageNotFoundErrorService implements Service {
+    @Override
+    public void perform(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ParseException, SQLException {
+        //response.sendRedirect(PAGE_NOT_FOUND_ERROR_PAGE);
+        RequestDispatcher requestDispatcher;
+        requestDispatcher = request.getRequestDispatcher(PAGE_NOT_FOUND_ERROR_PAGE);
+        requestDispatcher.forward(request,response);
+    }
+}
